21700153f05359f7b2a1822459aa64ce920d9f75
sgilley <sgilley@microsoft.com>
2017-04-27 11:04:09 -0500

    update

--- R/install_local.R
+++ R/install_local.R
@@ -2,15 +2,15 @@
 # This will install the packages needed for this solution
 # This has already been run on your VM if you have deployed the solution from Cortana Intelligence Gallery.
 
-install.packages("smbinning", lib="C:/Program Files/Microsoft/R Server/R_SERVER/library")
-install.packages("sqldf", lib="C:/Program Files/Microsoft/R Server/R_SERVER/library")
-install.packages("gsubfn", lib="C:/Program Files/Microsoft/R Server/R_SERVER/library")
-install.packages("proto", lib="C:/Program Files/Microsoft/R Server/R_SERVER/library")
-install.packages("RSQLite", lib="C:/Program Files/Microsoft/R Server/R_SERVER/library")
-install.packages("DBI", lib="C:/Program Files/Microsoft/R Server/R_SERVER/library")
-install.packages("partykit", lib="C:/Program Files/Microsoft/R Server/R_SERVER/library")
-install.packages("Formula", lib="C:/Program Files/Microsoft/R Server/R_SERVER/library")
-install.packages("ROCR", lib="C:/Program Files/Microsoft/R Server/R_SERVER/library")
-install.packages("chron", lib="C:/Program Files/Microsoft/R Server/R_SERVER/library")
+install.packages("smbinning")
+install.packages("sqldf")
+install.packages("gsubfn")
+install.packages("proto")
+install.packages("RSQLite")
+install.packages("DBI")
+install.packages("partykit")
+install.packages("Formula")
+install.packages("ROCR")
+install.packages("chron")
 
 print("Done installing packages")
