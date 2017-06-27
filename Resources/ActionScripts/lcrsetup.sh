635bb4299e765a3eaad5ea03b89cc4bc21d392c1
sgilley <sgilley@microsoft.com>
2017-06-27 16:59:59 -0500

    substitute pw

--- Resources/ActionScripts/lcrsetup.sh
+++ Resources/ActionScripts/lcrsetup.sh
@@ -4,8 +4,8 @@
 git clone  -b spark --single-branch  https://github.com/Microsoft/r-server-loan-credit-risk.git  loans
 cp loans/RSparkCluster/* /home/$1
 chmod 777 /home/$1/*.R
-rm -rf loans
-
+rm -rf loans-s 
+sed -i 's/SQLPW/$2/g' /home/$1/*.R
 
 # Configure edge node as one-box setup for R Server Operationalization
 /usr/local/bin/dotnet /usr/lib64/microsoft-r/rserver/o16n/9.1.0/Microsoft.RServer.Utils.AdminUtil/Microsoft.RServer.Utils.AdminUtil.dll -silentoneboxinstall "$2"
