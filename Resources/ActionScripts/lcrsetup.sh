09abd094e785806dab1265cbae0c1b88c7fcf06e
sgilley <sgilley@microsoft.com>
2017-07-10 08:31:51 -0500

    turn off telemetry

--- Resources/ActionScripts/lcrsetup.sh
+++ Resources/ActionScripts/lcrsetup.sh
@@ -9,3 +9,7 @@ sed -i "s/XXYOURSQLPW/$2/g" /home/$1/*.R
 
 # Configure edge node as one-box setup for R Server Operationalization
 /usr/local/bin/dotnet /usr/lib64/microsoft-r/rserver/o16n/9.1.0/Microsoft.RServer.Utils.AdminUtil/Microsoft.RServer.Utils.AdminUtil.dll -silentoneboxinstall "$2"
+
+# turn off telemetry 
+sed -i 's/options(mds.telemetry=1)/options(mds.telemetry=0)/g' /usr/lib64/microsoft-r/3.3/lib64/R/etc/Rprofile.site
+sed -i 's/options(mds.logging=1)/options(mds.logging=0)/g' /usr/lib64/microsoft-r/3.3/lib64/R/etc/Rprofile.site
