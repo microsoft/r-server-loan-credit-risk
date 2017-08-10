de80a5b97e017019721b5feb3108ae42b1b644da
v-robwhi <v-robwhi@microsoft.com>
2017-08-10 15:35:06 -0700

    still debugging path

--- Resources/ActionScripts/configureSolution.ps1
+++ Resources/ActionScripts/configureSolution.ps1
@@ -64,7 +64,7 @@ Start-Service SQLSERVERAGENT
 Write-Host -ForegroundColor 'Cyan' "Done switching SQL Server to Mixed Mode"
 
 ##cd $basedir\SQLR testing 
-cd $checkoutDir\SQLR
+cd $solutionBase\SQLR
 Write-Host -ForegroundColor 'Cyan' "Creating database user"
 
 # Variables to pass to createuser.sql script
