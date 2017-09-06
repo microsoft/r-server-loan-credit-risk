01d43c379566248aa5b0fbd06f471ddcfcde8325
v-robwhi <v-robwhi@microsoft.com>
2017-09-06 11:34:35 -0700

    comiiting setup psq

--- Resources/ActionScripts/configureSolution.ps1
+++ Resources/ActionScripts/configureSolution.ps1
@@ -102,7 +102,7 @@ Write-Host -ForegroundColor 'Cyan' "Done creating database user"
 $Inputfile = $solutionpath + "\Loan_Credit_Risk.ps1"
 # Run the solution
 #$Inputfile -ServerName $env:COMPUTERNAME -DBName $dbname -username $sqlUsername -password $sqlPassword  -uninterrupted y
-\Loan_Credit_Risk.ps1 -ServerName $env:COMPUTERNAME -DBName $dbname -username $sqlUsername -password $sqlPassword  -uninterrupted "Y" -is_production "N"
+.\Loan_Credit_Risk.ps1 -ServerName $env:COMPUTERNAME -DBName $dbname -username $sqlUsername -password $sqlPassword  -uninterrupted "Y" -is_production "N"
 #.\Loan_Credit_Risk.ps1 -ServerName $env:COMPUTERNAME -DBName $dbname -username $sqlUsername -password $sqlPassword  -uninterrupted y
 
 # copy Jupyter Notebook files
