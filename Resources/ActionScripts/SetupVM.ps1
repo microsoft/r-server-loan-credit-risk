9b81c4646a3125ea436e5fa10a26b1fefd69c466
v-robwhi <v-robwhi@microsoft.com>
2018-01-23 14:29:10 -0800

    changed git branch

--- Resources/ActionScripts/SetupVM.ps1
+++ Resources/ActionScripts/SetupVM.ps1
@@ -34,7 +34,7 @@ $Shortcut = "SolutionHelp.url"
 
 
 ### DON'T FORGET TO CHANGE TO MASTER LATER...
-$Branch = "dev" 
+$Branch = "dev2" 
 $InstallR = 'Yes'  ## If Solution has a R Version this should be 'Yes' Else 'No'
 $InstallPy = 'No' ## If Solution has a Py Version this should be 'Yes' Else 'No'
 $SampleWeb = 'No' ## If Solution has a Sample Website  this should be 'Yes' Else 'No' 
