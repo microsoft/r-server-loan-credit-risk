cb2af0b300a4d2b50686471c6d8b93b978f261d1
v-robwhi <v-robwhi@microsoft.com>
2018-01-23 15:18:38 -0800

    fixed SQL Config

--- Resources/ActionScripts/ConfigureSQL.ps1
+++ Resources/ActionScripts/ConfigureSQL.ps1
@@ -35,7 +35,7 @@ WRITE-HOST " ServerName set to $ServerName"
 
 $db = if ($Prompt -eq 'Y') {Read-Host  -Prompt "Enter Desired Database Base Name"} else {$SolutionName} 
 
-$dataList = ("Borrower", "Loan")
+$dataList = ("Borrower", "Loan", "Borrower_Prod", "Loan_Prod")
 
 
 
