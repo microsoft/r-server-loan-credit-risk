2707d5cde79dd74874d5be296b456425818baad6
Bob White <v-robwhi@microsoft.com>
2018-01-26 10:48:55 -0800

    bcping

--- Resources/ActionScripts/ConfigureSQL.ps1
+++ Resources/ActionScripts/ConfigureSQL.ps1
@@ -163,6 +163,8 @@ $RStart = Get-Date
 
         invoke-expression "bcp Borrower in C:\Solutions\Loans\Data\Borrower.txt -S $ServerName -d $dbName -T -q -k -c"
         invoke-expression "bcp Loan in C:\Solutions\Loans\Data\Loan.txt -S $ServerName -d $dbName -T -q -k -c"
+        invoke-expression "bcp Borrower_Prod in C:\Solutions\Loans\Data\Borrower.txt -S $ServerName -d $dbName -T -q -k -c"
+        invoke-expression "bcp Loan_Prod in C:\Solutions\Loans\Data\Loan.txt -S $ServerName -d $dbName -T -q -k -c"
         
 
 
