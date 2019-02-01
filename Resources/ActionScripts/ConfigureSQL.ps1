96a3c95e038b802b432ee9e6be89b2517a06578d
Jeroen ter Heerdt <jterh@microsoft.com>
2019-02-01 14:18:40 -0800

    Updating ConfigureSQL.ps1 bcp

--- Resources/ActionScripts/ConfigureSQL.ps1
+++ Resources/ActionScripts/ConfigureSQL.ps1
@@ -146,9 +146,9 @@ $RStart = Get-Date
 
 		# upload csv files into SQL tables
         Write-Host("bcp Borrower")
-        invoke-expression "bcp Borrower in C:\Solutions\Loans\Data\Borrower.txt -S $ServerName -d $dbName -T -c"
+        invoke-expression "bcp Borrower in C:\Solutions\Loans\Data\Borrower.txt -S $ServerName -d $dbName -T -c -F 2"
         Write-Host("bcp Loan")
-        invoke-expression "bcp Loan in C:\Solutions\Loans\Data\Loan.txt -S $ServerName -d $dbName -T -c"
+        invoke-expression "bcp Loan in C:\Solutions\Loans\Data\Loan.txt -S $ServerName -d $dbName -T -c -F 2"
         Write-Host("bcp Borrower_Prod. Fails")
         invoke-expression "bcp Borrower_Prod in C:\Solutions\Loans\Data\Borrower_Prod.txt -S $ServerName -d $dbName -T -k -c -F 2"
         Write-Host("bcp Loan_Prod. Fails")
